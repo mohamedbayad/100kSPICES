402cf13bb5cb418cdc334597e56fcfe296af5852
HananeAJDI <hananeajdi4@gmail.com>
2024-04-01 16:02:59 +0000

    01/04/24

--- base/urls.py
+++ base/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import login, register, logout, validate_registration
+from .views import login, register, logout, check_user, dashboard
 
 urlpatterns = [
     
     path("login/", login, name="login"),
     path("register/", register, name="register"),
     path("logout/", logout, name="logout"),
-    path("validate_registration/", validate_registration, name="validate_registration"),
-    
+    path("check_user/", check_user, name="check_user"),
+    path("dashboard/", dashboard, name="dashboard"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,29 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from django.contrib.auth import authenticate, logout as logout_view , login as login_view
 from django.contrib.auth.models import User
 
-
-
-
 # Create your views here.
 
 template_path = "authentication/"
-
-def validate_registration(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        password_confirmation = request.POST.get('password_confirmation')
-        
-        checkUser = User.objects.filter(username=username).exists()
-        
-        if checkUser:
-            return JsonResponse({"message": "the user is already registered", "from": "username"})
-        
-        if password != password_confirmation:
-            return JsonResponse({"message": "passwords do not match", "from": "password_confirmation"})
-    return render(request, f'{template_path}register.html')
 
 def login(request):
     if request.method == 'POST':
@@ -34,31 +16,35 @@ def login(request):
             login_view(request, user)
             return JsonResponse({"status": True, "message": "Login successful"})
         else:
-            return JsonResponse({"status": True, "message": "login failed"})
+            return JsonResponse({"status": False, "message": "login failed"})
     return render(request, f"{template_path}login.html", {'title' : "login"})
+
+def check_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        checkUser = User.objects.filter(username=username).exists()
+        if checkUser:
+            return JsonResponse({"exists": True, "message": "User already exists"})
+    return JsonResponse({"exists": False})
 
 def register(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password')
-        # password_confirmation = request.POST.get('password_confirmation')
+        password_confirmation = request.POST.get('password_confirmation')
         
-        # checkUser = User.objects.filter(username=username).exists()
-        
-        # if checkUser:
-        #     return JsonResponse({"message": "the user is already registered", "from": "username"})
-        
-        # if password != password_confirmation:
-        #     return JsonResponse({"message": "passwords do not match", "from": "password_confirmation"})
-        
-        user = User.objects.create_user(username, email, password)
-        user.save()
-
+        if password_confirmation == password:
+            user = User.objects.create_user(username, email, password)
+            user.save()
         
     return render(request, f'{template_path}register.html')
 
 def logout(request):
     logout_view(request)
-    
     return HttpResponse('logout successful')
+
+
+def dashboard(request):
+    
+    return HttpResponse('dashboard successful')
